61d6408ec5ebf598fe19df52d8bf13635aded962
Your Name <you@example.com>
2022-01-17 16:31:40 +0900

    django course 9 commit

--- accountapp/views.py
+++ accountapp/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 # Create your views here.
 
 def hello_world(request):
-    return render(request, 'base.html')
+    return render(request, 'accountapp/hello_world.html')
 
 #render는 Templates에서 response를 가져옴
 
