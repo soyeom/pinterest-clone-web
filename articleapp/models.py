b95cb70ab71a80b35bcac619724183926a6fe9d0
Your Name <you@example.com>
2022-01-29 18:02:22 +0900

    django course 36 commit

--- articleapp/models.py
+++ articleapp/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+class Article(models.Model):
+    writer = models.ForeignKey(User, on_delete=models.SET_NULL, related_name='article', null=True)
+    #지우면 게시글이 사라지지 않고 주인없는 게시글이 됨
+    title = models.CharField(max_length=200, null=True)
+    image = models.ImageField(upload_to='article/', null=False)
+    content = models.TextField(null=True)
+
+    created_at = models.DateTimeField(auto_now_add=True, null=True)
