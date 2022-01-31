452d685e036daaa7ac883199eb75625e47eaf5a5
Your Name <you@example.com>
2022-01-31 22:08:40 +0900

    django course 41 commit

--- articleapp/models.py
+++ articleapp/models.py
@@ -2,10 +2,14 @@ from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+from projectapp.models import Project
+
 
 class Article(models.Model):
     writer = models.ForeignKey(User, on_delete=models.SET_NULL, related_name='article', null=True)
     #지우면 게시글이 사라지지 않고 주인없는 게시글이 됨
+    project = models.ForeignKey(Project, on_delete=models.SET_NULL, related_name='article', null=True)
+
     title = models.CharField(max_length=200, null=True)
     image = models.ImageField(upload_to='article/', null=False)
     content = models.TextField(null=True)
--- pragmatic/urls.py
+++ pragmatic/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('profiles/', include('profileapp.urls')),
     path('articles/', include('articleapp.urls')),
     path('comments/', include('commentapp.urls')),
+    path('projects/', include('projectapp.urls')),
 
     *static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT),
     #슬래시를 꼭 적어줘야 주소창이 뜬다
